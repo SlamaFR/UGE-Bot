9a1d4841a30094db4255a6d5493adc43b66ad870
Irwin Madet <irwin.madet@gmail.com>
2021-06-07 18:32:02 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+group = "io.slama"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://m2.dv8tion.net/releases")
+        name = "m2-dv8tion"
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("net.dv8tion:JDA:4.2.1_270") {
+        exclude("opus-java")
+    }
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "UGEBot_v2"
+
