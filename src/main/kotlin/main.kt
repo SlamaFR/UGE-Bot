4e316ba2827abe3df9ab006a9c072a9eed28cadd
Irwin Madet <irwin.madet@gmail.com>
2021-06-12 19:13:22 +0200

    Rework 'reload' console command

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,6 +5,10 @@ import commands.AutoRoleCommand
 import commands.CallCommand
 import events.loadAutoRoles
 import commands.KevalCommand
+import core.clearGuildConfigs
+import core.registerGlobalCommands
+import core.registerGuildCommands
+import events.clearAutoRoles
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.events.ReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
@@ -12,18 +16,12 @@ import net.dv8tion.jda.api.requests.GatewayIntent
 import net.dv8tion.jda.api.utils.ChunkingFilter
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import utils.addCommands
 import java.io.File
 import kotlin.system.exitProcess
 
-val logger: Logger = LoggerFactory.getLogger("UGEBot")
-
-val token = Key("token", stringType)
-val config = ConfigurationProperties.fromFile(File("bot.properties"))
-
-fun main() {
-    UGEBot(config[token])
-}
+private val logger: Logger = LoggerFactory.getLogger("UGEBot")
+private val token = Key("token", stringType)
+private val config = ConfigurationProperties.fromFile(File("bot.properties"))
 
 class UGEBot(token: String) : ListenerAdapter() {
 
@@ -44,14 +42,20 @@ class UGEBot(token: String) : ListenerAdapter() {
     }
 
     override fun onReady(event: ReadyEvent) {
-        load()
         jda.addEventListener(CallCommand(), KevalCommand(), AutoRoleCommand())
-        jda.guildCache.forEach {
-            it.loadAutoRoles()
-        }
+        load()
     }
 
     private fun load() {
+        clearAutoRoles()
+        clearGuildConfigs()
+        jda.guildCache.forEach { it.loadAutoRoles() }
+        jda.registerGlobalCommands()
+        jda.guilds.forEach { it.registerGuildCommands() }
         logger.info("Registered commands")
     }
+}
+
+fun main() {
+    UGEBot(config[token])
 }
