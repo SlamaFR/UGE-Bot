4fb78ddb33cb693c998d3247ccfc47fe041b68b5
Irwin Madet <irwin.madet@gmail.com>
2021-06-28 02:21:20 +0200

    Rename strings.kt.kt and add methods for longs

--- src/main/kotlin/utils/strings.kt
+++ src/main/kotlin/utils/strings.kt
@@ -6,3 +6,10 @@ fun String.pluralize(count: Int) =
 fun String.pluralize(count: Int, form: String?) =
     if (count > 1) form ?: this + "s"
     else this
+
+fun String.pluralize(count: Long) =
+    pluralize(count, null)
+
+fun String.pluralize(count: Long, form: String?) =
+    if (count > 1) form ?: this + "s"
+    else this
