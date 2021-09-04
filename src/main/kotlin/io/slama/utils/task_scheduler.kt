efe674cf010f2d7ae3120457f4c51a391cd6bf23
notKamui <teillard.jimmy@outlook.fr>
2021-09-04 14:10:13 +0200

    Dedicated the scheduler to IO Dispatchers.

--- src/main/kotlin/io/slama/utils/task_scheduler.kt
+++ src/main/kotlin/io/slama/utils/task_scheduler.kt
@@ -1,22 +1,20 @@
 package io.slama.utils
 
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
 object TaskScheduler {
-    @OptIn(DelicateCoroutinesApi::class)
     fun later(delay: Long, unit: TimeUnit, block: () -> Unit) =
-        GlobalScope.launch {
+        CoroutineScope(Dispatchers.IO).launch {
             delay(unit.toMillis(delay))
             block()
         }
 
-    @OptIn(DelicateCoroutinesApi::class)
     fun async(block: () -> Unit) =
-        GlobalScope.launch {
+        CoroutineScope(Dispatchers.IO).launch {
             block()
         }
 }
