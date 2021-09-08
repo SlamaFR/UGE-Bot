3e0af24a094588b16a0165e5dba2308c4d222db4
Irwin Madet <irwin.madet@gmail.com>
2021-09-08 12:37:18 +0200

    Add ability to send reply on message channel

--- src/main/kotlin/io/slama/utils/replies.kt
+++ src/main/kotlin/io/slama/utils/replies.kt
@@ -1,12 +1,25 @@
 package io.slama.utils
 
 import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.entities.MessageChannel
+import net.dv8tion.jda.api.entities.TextChannel
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
+import net.dv8tion.jda.api.requests.restaction.MessageAction
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction
 import java.awt.Color
 
 fun SlashCommandEvent.replySuccess(message: String): ReplyAction {
     return this.replyEmbeds(
+        EmbedBuilder()
+            .setTitle("Succès")
+            .setDescription(message)
+            .setColor(Color(0x2ecc71))
+            .build()
+    )
+}
+
+fun MessageChannel.sendSuccess(message: String): MessageAction {
+    return this.sendMessage(
         EmbedBuilder()
             .setTitle("Succès")
             .setDescription(message)
@@ -25,8 +38,28 @@ fun SlashCommandEvent.replyWarning(message: String): ReplyAction {
     )
 }
 
+fun MessageChannel.sendWarning(message: String): MessageAction {
+    return this.sendMessage(
+        EmbedBuilder()
+            .setTitle("Avertissement")
+            .setDescription(message)
+            .setColor(Color(0xf1c40f))
+            .build()
+    )
+}
+
 fun SlashCommandEvent.replyError(message: String): ReplyAction {
     return this.replyEmbeds(
+        EmbedBuilder()
+            .setTitle("Erreur")
+            .setDescription(message)
+            .setColor(Color(0xe74c3c))
+            .build()
+    )
+}
+
+fun MessageChannel.sendError(message: String): MessageAction {
+    return this.sendMessage(
         EmbedBuilder()
             .setTitle("Erreur")
             .setDescription(message)
