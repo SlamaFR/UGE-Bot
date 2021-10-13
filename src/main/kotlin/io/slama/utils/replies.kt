2320a0bc09dc221753a7d04d4c2be70b878c0296
Irwin Madet <irwin.madet@gmail.com>
2021-10-13 09:02:47 +0200

    Update colors in replies

--- src/main/kotlin/io/slama/utils/replies.kt
+++ src/main/kotlin/io/slama/utils/replies.kt
@@ -10,9 +10,9 @@ import net.dv8tion.jda.api.requests.restaction.interactions.ReplyAction
 import java.awt.Color
 
 object EmbedColors {
-    val PURPLE = Color(0x5913E0)
-    val VIOLET = Color(0x9B59B6)
-    val BLUE = Color(0x09b6c2)
+    val PURPLE = Color(0x8e44ad)
+    val VIOLET = Color(0x9b59b6)
+    val BLUE = Color(0x3498db)
     val GREEN = Color(0x2ecc71)
     val YELLOW = Color(0xf1c40f)
     val ORANGE = Color(0xe67e22)
